063d5ffbeab9e7d79c3671e5f5da04982b83142e
Blackwave <r00t.pro@hotmail.com>
2014-07-12 20:20:05 +0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2014 Blackwave <blackwave-rt@hotmail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from distutils.core import setup
+
+setup(
+	name='pikabu_api',
+	version='0.0.1',
+	description='Неофициальная библиотека имитирующее неофициальное апи для pikabu.ru',
+	author='Blackwave',
+	author_email='blackwave-rt@hotmail.com',
+	url='https://github.com/kafeman/habrahabr-python',
+	py_modules=['pikabu'],
+	)
