4329b3963717a5dc760e5a0d7e8eab9d7e2d66cc
Blackwave <r00t.pro@hotmail.com>
2014-07-13 08:36:03 +0600

    Добавлены новые функции
    Добавлено:
    Поиск по сайту
    Поиск по тегам
    Исправлено:
    Исправлена ошибка с авторизацией, вывод ошибки с траблом при авторизации.

--- pikabu.py
+++ pikabu.py
@@ -13,6 +13,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 import cookielib
 import urllib2, urllib
 import json
@@ -37,12 +38,16 @@ def fetch_url(_url, settings=None):
 	                   	'remember'	:	0
 					}
 		resp 		= req.open(url, urllib.urlencode(headers))
-		response 	= json.loads(resp.read())
-		#if json.loads(resp.read())["logined"] == 0:
-		#	raise ValueError('Неверно указан логин или пароль')
+		response 	= json.loads(resp.read().decode("windows-1251"))
+		if int(response["logined"]) == 0:
+			print 'Неверно указан логин или пароль'
+			sys.exit(1)
+		if int(response["logined"]) == -1:
+			print response['error']
+			sys.exit(1)
 
 	if _url is not None:
-		resp = req.open(ENDPOINT + _url)
+		resp = req.open(ENDPOINT + _url)		
 		return resp.read()
 	else: return False
 
@@ -57,8 +62,6 @@ class PikaService:
 			return fetch_url(url, self.settings)
 		else: return False
 
-
-
 class PikabuSearch(PikaService):
 	def search_posts(self, query, page=1):
 		return self.request('/search/posts/%s?page=%d' % (query, page))
@@ -68,7 +71,10 @@ class PikabuPosts(PikaService):
 		_page = self.request('%s?page=%s&twitmode=1' % (query, page))
 		if _page is not None:
 			posts_list = []	
-			page_body = lxml.html.document_fromstring(json.loads(_page.decode("windows-1251"))["html"].replace("<br>", "\\n"))
+			try:
+				page_body = lxml.html.document_fromstring(json.loads(_page.decode("windows-1251"))["html"].replace("<br>", "\\n"))
+			except:
+				return False			
 			for post_id in json.loads(_page.decode("windows-1251"))["news_arr"]:				
 				post_title  	= page_body.xpath('//*[@id="num_dig3%s"]'%post_id)[0].text
 				post_url		= page_body.xpath('//*[@id="num_dig3%s"]'%post_id)[0].get("href")
@@ -89,6 +95,84 @@ class PikabuPosts(PikaService):
 				post_tags			= [result.text for result in page_body.xpath('//*[@id="story_table_%s"]//tr/td[2]/table[@id="story_main_t"]//tr/td/div[3]/span[1]/span'%post_id)]
 				posts_list.append(ObjectPosts(post_id, post_title, post_url, post_text, post_image, post_desc, post_author, post_time, post_comments, post_rating, post_tags))
 			return posts_list
+
+	def search(self, query, cur_page=1, in_hot=True, in_pics=True, in_text=True, in_video=True):
+		if query is not None:
+			if in_hot: added_params = "&hot_search=on"
+			if in_pics: added_params += "&pic_search=on"
+			if in_text: added_params += "&text_search=on"
+			if in_video: added_params += "&video_search=on"
+			added_params += "&page=%s"%cur_page
+			_page = self.request('search.php?q=%s%s' % (query, added_params))
+			posts_list = []
+			page_body = lxml.html.document_fromstring(_page.decode("windows-1251"))
+			for story in page_body.xpath('//*[@id="stories_container"]/table'):
+				try:
+					post_id 		= int(story.get("attr"))
+					post_title  	= page_body.xpath('//*[@id="num_dig3%s"]'%post_id)[0].text
+					post_url		= page_body.xpath('//*[@id="num_dig3%s"]'%post_id)[0].get("href")
+					post_desc 		= page_body.xpath('//*[@id="story_table_%s"]//tr/td[2]/table[@id="story_main_t"]//tr/td/div[2]'%post_id)[0].text
+					post_image  	= page_body.cssselect('table#story_table_%s'%post_id)[0].get("lang")
+					try:
+						post_text 	= " ".join(page_body.xpath('//*[@id="textDiv%s"]/text()'%post_id))
+					except: post_text = None
+					try:
+						post_author 	= page_body.xpath('//*[@id="story_table_%s"]//tr/td[2]/table[@id="story_main_t"]//tr/td/div[3]/span[1]/a[3]'%post_id)[0].text
+						post_time 		= page_body.xpath('//*[@id="story_table_%s"]//tr/td[2]/table[@id="story_main_t"]//tr/td/div[3]/span[1]/a[4]'%post_id)[0].text
+						post_comments	= page_body.xpath('//*[@id="story_table_%s"]//tr/td[2]/table[@id="story_main_t"]//tr/td/div[3]/span[1]/a[2]'%post_id)[0].text
+					except:
+						post_author 	= page_body.xpath('//*[@id="story_table_%s"]//tr/td[2]/table[@id="story_main_t"]//tr/td/div[2]/span[1]/a[3]'%post_id)[0].text
+						post_time 		= page_body.xpath('//*[@id="story_table_%s"]//tr/td[2]/table[@id="story_main_t"]//tr/td/div[2]/span[1]/a[4]'%post_id)[0].text
+						post_comments	= page_body.xpath('//*[@id="story_table_%s"]//tr/td[2]/table[@id="story_main_t"]//tr/td/div[2]/span[1]/a[2]'%post_id)[0].text			
+					try:
+						post_rating 	= page_body.xpath('//*[@id="num_digs%s"]'%post_id)[0].text
+					except:
+						post_rating		= None				
+					post_tags			= [result.text for result in page_body.xpath('//*[@id="story_table_%s"]//tr/td[2]/table[@id="story_main_t"]//tr/td/div[3]/span[1]/span'%post_id)]
+					posts_list.append(ObjectPosts(post_id, post_title, post_url, post_text, post_image, post_desc, post_author, post_time, post_comments, post_rating, post_tags))					
+				except:
+					pass
+			return posts_list
+		else: return False
+
+	def tag(self, tag_name, page=1, category="new"):
+		if tag_name and category is not None:
+			_page = self.request('tag/%s/%s?page=%s&twitmode=1' % (tag_name, category, page))
+			if _page is not None:
+				posts_list = []
+				print _page
+				try:
+					page_body = lxml.html.document_fromstring(json.loads(_page.decode("windows-1251"))["html"].replace("<br>", "\\n"))
+				except:
+					return False
+				for post_id in json.loads(_page.decode("windows-1251"))["news_arr"]:				
+					post_title  	= page_body.xpath('//*[@id="num_dig3%s"]'%post_id)[0].text
+					post_url		= page_body.xpath('//*[@id="num_dig3%s"]'%post_id)[0].get("href")
+					post_text		= page_body.xpath('//*[@id="story_table_%s"]//tr/td[2]/table[@id="story_main_t"]//tr/td/div[2]'%post_id)[0].text
+					post_image  	= page_body.cssselect('table#story_table_%s'%post_id)[0].get("lang")
+					try:
+						post_desc 	= " ".join(page_body.xpath('//*[@id="textDiv%s"]/text()'%post_id))
+					except: post_desc = None
+					try:
+						post_author 	= page_body.xpath('//*[@id="story_table_%s"]//tr/td[2]/table[@id="story_main_t"]//tr/td/div[3]/span[1]/a[3]'%post_id)[0].text
+						post_time 		= page_body.xpath('//*[@id="story_table_%s"]//tr/td[2]/table[@id="story_main_t"]//tr/td/div[3]/span[1]/a[4]'%post_id)[0].text
+						post_comments	= page_body.xpath('//*[@id="story_table_%s"]//tr/td[2]/table[@id="story_main_t"]//tr/td/div[3]/span[1]/a[2]'%post_id)[0].text
+					except:
+						post_author 	= page_body.xpath('//*[@id="story_table_%s"]//tr/td[2]/table[@id="story_main_t"]//tr/td/div[2]/span[1]/a[3]'%post_id)[0].text
+						post_time 		= page_body.xpath('//*[@id="story_table_%s"]//tr/td[2]/table[@id="story_main_t"]//tr/td/div[2]/span[1]/a[4]'%post_id)[0].text
+						post_comments	= page_body.xpath('//*[@id="story_table_%s"]//tr/td[2]/table[@id="story_main_t"]//tr/td/div[2]/span[1]/a[2]'%post_id)[0].text			
+					try:
+						post_rating 	= page_body.xpath('//*[@id="num_digs%s"]'%post_id)[0].text
+					except:
+						post_rating		= None		
+					post_tags			= [result.text for result in page_body.xpath('//*[@id="story_table_%s"]//tr/td[2]/table[@id="story_main_t"]//tr/td/div[3]/span[1]/span'%post_id)]
+					posts_list.append(ObjectPosts(post_id, post_title, post_url, post_text, post_image, post_desc, post_author, post_time, post_comments, post_rating, post_tags))
+				return posts_list
+			else: return False
+		else: return False		
+
+
+
 
 class PikabuComments(PikaService):
 	def get(self, post_id, post_url):
@@ -113,7 +197,6 @@ class PikabuComments(PikaService):
 					comment_list.append( ObjectComments(comment_id, comment_rating, comment_author, comment_time, comment_text) )
 				return comment_list
 		else: return False
-		#[result.text for result in page_body.xpath('//*[@class="comment_desc"]')]		
 
 class PikabuTopTags(PikaService):
 	def get(self, limit=10):
@@ -222,7 +305,10 @@ class PikabuProfile(PikaService):
 					self._awards.append(cur_award.get("title"))
 			else: pass
 		else:pass
-		return self._awards					
+		return self._awards
+
+	def set(self, arg, value):
+		pass				
 
 class PikabuUserInfo(PikaService):
 	def __init__(self, **settings):
@@ -305,8 +391,6 @@ class PikabuUserInfo(PikaService):
 			else: pass
 		else:pass
 		return self._awards
-
-
 
 class ObjectPosts():
 	def __init__(self, _id, title, url, description, image, text, author, time, comment, rating, tags):
